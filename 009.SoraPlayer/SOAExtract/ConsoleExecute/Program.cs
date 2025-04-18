bcfe88a341a9ea7fbc7d01273aef76dc4945d123
shikwi <crpandas@163.com>
2025-04-18 17:52:50 +0800

    [soraplayer]锈翅

--- 009.SoraPlayer/SOAExtract/ConsoleExecute/Program.cs
+++ 009.SoraPlayer/SOAExtract/ConsoleExecute/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Linq;
 using SoraPlayerStatic;
 
 namespace ConsoleExecute
@@ -9,7 +10,10 @@ namespace ConsoleExecute
 
         static void Main(string[] args)
         {
-            string[] packs = Directory.GetFiles("E:\\夏花的轨迹——That Summer Of Eternal Eden-体验版β", "*.soa");
+            string[] packs = Directory.GetFiles("F:\\SteamLibrary\\steamapps\\common\\锈翅", "*.soa");
+                            //.Concat(Directory.GetFiles("E:\\夏花的轨迹——That Summer Of Eternal Eden - 体验版β", " *.soa"))
+                            //.ToArray();
+
 
             foreach(var packPath in packs)
             {
